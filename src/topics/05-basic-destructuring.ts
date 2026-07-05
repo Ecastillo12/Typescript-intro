018de3b0cb79ad5639be62028fdf3b07006fef06
Eduardo Castillo <e_castillo12@outlook.com>
2026-07-04 18:23:37 -0600

    continuacion del curso, migracion a pnpm

---
+++ src/topics/05-basic-destructuring.ts
@@ -0,0 +1,34 @@
+interface AudioPlayer {
+    audioVolume: number;
+    songDuration: number;
+    song: string;
+    details: Details
+}
+
+interface Details {
+    author: string;
+    year: number;
+}
+
+const audioPlayer: AudioPlayer = {
+    audioVolume: 90,
+    songDuration: 36,
+    song: "One Step Closer",
+    details: {
+        author: "Linkin Park",
+        year: 2001
+    }
+}
+
+const song = 'Breaking The Habit';
+
+const { song:anotherSong,
+        songDuration,
+        details
+} = audioPlayer;
+
+const { author } = details;
+
+console.log('Song: ', anotherSong);
+console.log('Song: ', songDuration);
+console.log('Song: ', author);
